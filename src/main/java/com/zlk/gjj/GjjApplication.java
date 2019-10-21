d8fe0d5297ebb79ec3c96b05f88da50816488484
HRY <2098506133@qq.com>
2019-10-21 10:41:09 +0800

    项目初始化配置

---
+++ src/main/java/com/zlk/gjj/GjjApplication.java
@@ -0,0 +1,13 @@
+package com.zlk.gjj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GjjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zlk/gjj/GjjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlk.gjj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GjjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
