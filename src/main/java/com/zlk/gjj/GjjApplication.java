83a4c9e24fbb60a17d59cbd9f86ba9cde99568c5
HRY <2098506133@qq.com>
2019-10-21 12:00:41 +0800

    添加修改配置文件

--- src/main/java/com/zlk/gjj/GjjApplication.java
+++ src/main/java/com/zlk/gjj/GjjApplication.java
@@ -1,9 +1,11 @@
 package com.zlk.gjj;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.zlk.gjj.registerAndAccount.*.mapper")
 public class GjjApplication {
 
     public static void main(String[] args) {
