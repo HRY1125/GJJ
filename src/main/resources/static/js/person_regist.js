5849b068657a0488bc616f8f10d56678788e49c6
HRY <2098506133@qq.com>
2019-10-23 13:49:37 +0800

    修复个人登记国籍bug

--- src/main/resources/static/js/person_regist.js
+++ src/main/resources/static/js/person_regist.js
@@ -7,8 +7,10 @@ window.onload=function(){
         form.on('select(papers)', function(data){
             if ($("#select1").val()==1){
                 $("#div_nation").css("display","block");
+                $("#ipt_nation").attr("required","required");
             }else{
                 $("#div_nation").css("display","none");
+                $("#ipt_nation").removeAttr("required");
             }
         });
     });
