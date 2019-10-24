4f00ce295d11d9e2ae945fbd04cee36e340059f6
zhang <3230256690@qq.com>
2019-10-24 14:51:45 +0800

    添加util包

--- src/main/java/com/zlk/gjj/registerAndAccount/util/IdUtils.java
+++ src/main/java/com/zlk/gjj/registerAndAccount/util/IdUtils.java
@@ -1,13 +1,41 @@
 package com.zlk.gjj.registerAndAccount.util;
 
 import java.util.Date;
+import java.util.UUID;
 
+/**
+ * @program: gjj
+ * @description: unitId生成工具类
+ * @author: zyx
+ * @create: 2019-10-22 15:55
+ */
 public class IdUtils {
-    //自动生成Id
+
+    /**
+     *  根据时间戳生成i位数字id
+     *@method getUnitId
+     *@params [i]
+     *@return java.lang.String
+     *@author zhang
+     *@time 2019/10/23  17:29
+     */
     public static String getUnitId(int i){
-        long time=new Date().getTime();
-        String str=String.valueOf(time);
-        String unitId=str.substring(str.length() - i);
-        return  unitId;
+        long time = new Date().getTime();
+        String str = String.valueOf(time);
+        String unitId = str.substring(str.length() - i);
+        return unitId;
+    }
+
+    /**
+     *  生成加密过的UUid
+     *@method getUUID
+     *@params []
+     *@return java.lang.String
+     *@author zhang
+     *@time 2019/10/23  17:30
+     */
+    public static String getUUID(){
+        String id = UUID.randomUUID().toString().replaceAll("-", "");
+        return id;
     }
 }
