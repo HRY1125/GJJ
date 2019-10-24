4af14042b97639d3ea77ca88592e2d371c796f3f
HRY <2098506133@qq.com>
2019-10-24 09:16:15 +0800

    添加跳转controller

--- src/main/resources/static/js/pay_method.js
+++ src/main/resources/static/js/pay_method.js
@@ -1,5 +1,61 @@
 window.onload=function(){
 
+    layui.use('form', function(){
+        var form = layui.form;
 
+        form.on('radio(kind)', function(data){
+            var value = data.value;
+            if(value==0){
+                $(".entrust").css("display","block");
+                $(".remit").css("display","none");
+                $(".check").css("display","none");
+                $(".cash").css("display","none");
+                $(".finance").css("display","none");
+                $(".transfer").css("display","none");
+            }else if(value==1){
+                $(".entrust").css("display","none");
+                $(".remit").css("display","block");
+                $(".check").css("display","none");
+                $(".cash").css("display","none");
+                $(".finance").css("display","none");
+                $(".transfer").css("display","none");
+            }else if(value==2){
+                $(".entrust").css("display","none");
+                $(".remit").css("display","none");
+                $(".check").css("display","block");
+                $(".cash").css("display","none");
+                $(".finance").css("display","none");
+                $(".transfer").css("display","none");
+            }else if(value==3){
+                $(".entrust").css("display","none");
+                $(".remit").css("display","none");
+                $(".check").css("display","none");
+                $(".cash").css("display","block");
+                $(".finance").css("display","none");
+                $(".transfer").css("display","none");
+            }else if(value==4){
+                $(".entrust").css("display","none");
+                $(".remit").css("display","none");
+                $(".check").css("display","none");
+                $(".cash").css("display","none");
+                $(".finance").css("display","block");
+                $(".transfer").css("display","none");
+            }else if(value==5){
+                $(".entrust").css("display","none");
+                $(".remit").css("display","none");
+                $(".check").css("display","none");
+                $(".cash").css("display","none");
+                $(".finance").css("display","none");
+                $(".transfer").css("display","block");
+            }else{
+                $(".entrust").css("display","none");
+                $(".remit").css("display","none");
+                $(".check").css("display","none");
+                $(".cash").css("display","none");
+                $(".finance").css("display","none");
+                $(".transfer").css("display","none");
+            }
+        });
+    });
 
 };
